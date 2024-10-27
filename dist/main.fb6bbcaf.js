69d803debddaffa2ee1e3a04be80f6027eb1d522
rubenprez <33560196+rubenprez@users.noreply.github.com>
2024-10-26 21:03:22 -0700

    bt and video push testing
    putsh

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -219,7 +219,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49181" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52892" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
